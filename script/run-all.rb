3ae2d7fd555d7e2eb60cc08d60267b85fe4e6dd6
Thomas Nadin <nadinengland@gmail.com>
2012-05-14 09:33:44 +0100

    Initial Commit
    Runner script and first few solutions

---
+++ script/run-all.rb
@@ -0,0 +1,11 @@
+puts 'Executing Euler Solutions by Thomas Nadin (nadinengland@gmail.com)'
+
+# Handy little method to execute a ruby script with arguments
+def eval_script filename, arguments
+  proc = Proc.new {}
+  eval(File.read(filename), proc.binding, filename)
+end
+
+Dir["../solution/*.rb"].each do |s|
+  puts "#{s}: #{eval_script(s, nil)}"
+end
